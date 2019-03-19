0d5da74f43e57471243d45eff3c355f8450cef87
Unknown <nikolabjelo@gmail.com>
2019-03-19 13:37:55 +0100

    Publishing update mutations

--- src/schema/mutations/index.js
+++ src/schema/mutations/index.js
@@ -4,12 +4,16 @@ import {
 
 import CreateSignalMutation from './CreateSignal';
 import CreateOrderMutation from './CreateOrder';
+import UpdateSignalStateMutation from './UpdateSignalState';
+import UpdateOrderStateMutation from './UpdateOrderState';
 
 const Mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: Object.assign(
     CreateSignalMutation,
     CreateOrderMutation,
+    UpdateSignalStateMutation,
+    UpdateOrderStateMutation,
   ),
 });
 
